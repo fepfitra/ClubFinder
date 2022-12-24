35d6f170ac32f754cac45e1c985c750b1722f2d9
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-12-24 00:50:26 -0500

    convert to shadow dom

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,13 +1,27 @@
 import './club-item.js';
 
 class ClubList extends HTMLElement {
+  constructor (){
+    super();
+    this.shadowDOM = this.attachShadow({mode:'open'});
+  }
   set clubs(clubs){
     this._clubs = clubs;
     this.render();
   }
   
   render(){
-    this.innerHTML = ``;
+    this.shadowDWOM.innerHTML = `
+      <style>
+        .placeholder {
+          font-weight: lighter;
+          color: rgba(0, 0, 0, 0.5);
+          -webkit-user-select: none;
+          -noz-user-select: none;
+          user-select: none;
+        }
+      </style>
+    `;
     this._clubs.forEach(club => {
       const clubItemElement = document.createElement('club-item');
       clubItemElement.club = club;
